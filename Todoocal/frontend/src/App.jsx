8bb277d9c0c080ecfebcee82e2f05b456ad04220
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-12-30 23:15:15 +0530

    Completed_todo

--- Todoocal/frontend/src/App.jsx
+++ Todoocal/frontend/src/App.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { Todoprovider } from './Context/TodoContext'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, settodos] = useState([])
 
 
   const addTodo = (todo) => {
-    settodos((prev) => [...prev, { id: Date.now, ...todo }])
+    settodos((prev) => [...prev, { id: Date.now(), ...todo }])
   }
 
   const updateTodo = (id, todo) => {
-    settodos((prev) => {
-      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
-    })
+    settodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    )
 
   }
 
@@ -52,10 +53,20 @@ function App() {
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
-            {/* Todo form goes here */}
+            <TodoForm/>
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+
+            {
+              todos.map((todo) => (
+                <div key={todo.id}
+                className='w-full'
+                >
+                  <TodoItem  todo={todo} />
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
