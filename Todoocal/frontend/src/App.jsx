0deaad2049e505e699517c45be1a55836e09323d
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-12-30 22:04:32 +0530

    Todocontext

--- Todoocal/frontend/src/App.jsx
+++ Todoocal/frontend/src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Todoprovider } from './Context/TodoContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, settodos] = useState([])
+
+  const addTodo = (todo) =>{
+      settodos ((prev) => [...prev, {id:Date.now, ...todo}])
+  }
+
 
   return (
-    <>
-      <div className='W-30 h-20 text-6xl flex justify-center '> Todo</div>
-    </>
+    <Todoprovider value={{todos ,addTodo,updateTodo,deleteTodo,toggleTodo}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+        <div className="mb-4">
+          {/* Todo form goes here */}
+        </div>
+        <div className="flex flex-wrap gap-y-3">
+          {/*Loop and Add TodoItem here */}
+        </div>
+      </div>
+    </div>
+    </Todoprovider>
   )
 }
 
---
+++ Todoocal/frontend/src/Context/index.js
@@ -0,0 +1,1 @@
+import {useTodo,Todoprovider,TodoContext} from "./TodoContext.js"
