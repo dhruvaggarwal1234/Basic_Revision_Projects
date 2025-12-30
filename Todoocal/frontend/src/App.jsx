ef3f63cc36d3e06961f8d9c3c4ccb78ba3d8b753
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-12-30 21:30:36 +0530

    Setup_todo

---
+++ Todoocal/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='W-30 h-20 text-6xl flex justify-center '> Todo</div>
+    </>
+  )
+}
+
+export default App
