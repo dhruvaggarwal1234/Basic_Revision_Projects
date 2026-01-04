bbfbcf63637c372203ebbdbd2c79db7ab926bb00
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2026-01-04 20:52:57 +0530

    redux

---
+++ Redux/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+ 
+  return (
+    <>
+     <div className="w-full h-screen flex  items-center justify-center bg-black text-white">this is redux</div>
+    </>
+  )
+}
+
+export default App
